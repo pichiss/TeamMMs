92fd3cedced3a61499464ea220c09a444bcb8c26
keumju <keumju0409@gmail.com>
2024-07-23 17:34:01 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, createContext } from 'react'
 import { Routes, Route } from 'react-router-dom';
-// import { loginData, reducer } from './assets/component/page/login/loginData'
 
 import './reset.css'
 import './App.css'
@@ -13,22 +12,30 @@ import Join from './assets/component/page/login/Join'
 import Footer from './assets/component/footer/footer'
 import Profile from './assets/component/page/mypage/profile'
 
-// export const loginContext = createContext();
-// export const editContext = createContext();
 
 function App() {
+  const [page, setPage] = useState(true);
   
-
+  
   return (
     <>
-    <Header/>
-      <Routes>
-        <Route path="/TeamMMs/" element={<Main />}/>
-        <Route path="/TeamMMs/login" element={<Login />}/>
-        <Route path="/TeamMMs/join" element={<Join />}/>
-        <Route path="/TeamMMs/mypage" element={<Profile />}/>
-      </Routes>
-    <Footer />
+    {page == true ?
+    <>
+      <Header setPage={setPage}/>
+      {console.log(page)}
+        <Routes>
+          <Route path="/TeamMMs/" element={<Main />}/>
+          <Route path="/TeamMMs/login" element={<Login />}/>
+          <Route path="/TeamMMs/join" element={<Join />}/>
+          <Route path="/TeamMMs/mypage" element={<Profile />}/>
+        </Routes>
+      <Footer />
+    </>
+    :
+    <>
+      <div>학습하기 페이지 출력</div>
+    </>
+    }
     </>
   )
 }

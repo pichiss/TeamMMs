f855f17264d2f32c53e6fd21bfbf1b778d4a7132
mina <pichis1680@gmail.com>
2024-07-23 15:50:45 +0900

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './reset.css'
 import './App.css'
 
 import Footer from './assets/component/footer/footer'
+import Profile from './assets/component/page/mypage/profile'
 
 function App() {
 
   return (
     <>
-    <Footer />
+    <Profile />
     </>
   )
 }

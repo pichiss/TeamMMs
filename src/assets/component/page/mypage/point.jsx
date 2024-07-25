b2b0a8bc96c917ffddc0ed855d06ba4fe06861c1
pcm5433 <pcm5433@naver.com>
2024-07-25 17:49:02 +0900

    0725-5

--- src/assets/component/page/mypage/point.jsx
+++ src/assets/component/page/mypage/point.jsx
@@ -11,7 +11,24 @@ function Point(){
                 <Subnav key={1} tit={'학부모 코너'} />
                 <article className='pointWrap'>
                     <h2 className='subtit'>학부모 코너</h2>
-                    <h3>포인트 적립 내역</h3>
+                    <h3 className='bgBlue'>포인트 적립 내역</h3>
+                    <div className='pointHistory'></div>
+                    <table className="subscription">
+                            <thead>
+                                <tr className="bgBlue">
+                                    <td>사용내역</td>
+                                    <td>사용날짜</td>
+                                    <td>포인트</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>[GS25] 디지털상품권 1천원권</td>
+                                    <td>2024.07.18</td>
+                                    <td className="expend">-1000</td>
+                                </tr>
+                            </tbody>
+                        </table>
                 </article>
             </section>
         </>

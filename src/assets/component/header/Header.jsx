3fed13693cdd1f913fb66ef5cdd2937d42836ad7
keumju <keumju0409@gmail.com>
2024-07-25 16:15:26 +0900

    링크 완성ㅇㅇㅇ

--- src/assets/component/header/Header.jsx
+++ src/assets/component/header/Header.jsx
@@ -99,7 +99,9 @@ function Header({setPage, user, kakaoLogout}) {
                     <Link to='/mypage'><li>학부모코너</li></Link>
                 </ul>
                 {user ?
-                    <div>hello</div>
+                    <div className='subNavMyLog'>
+                        <Link to='/' onClick={kakaoLogout}><div>로그아웃</div></Link>
+                    </div>
                 :
                     <ul className='hamLogin'>
                         <Link to='/login'><li onClick={close}>로그인</li></Link>

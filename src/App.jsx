dfef1f6859b69913d103fbeaf1bc00e7ba625297
pcm5433 <pcm5433@naver.com>
2024-07-25 15:10:56 +0900

    0725

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import EduMain from './assets/component/page/edu/EduMain.jsx';
 import EduToday from './assets/component/page/edu/EduToday.jsx';
 import EduTodayCont from './assets/component/page/edu/EduTodayCont.jsx';
 import EduMathQ1 from './assets/component/page/edu/EduMath/EduMathQ1.jsx';
+import EduMathQ2 from './assets/component/page/edu/EduMath/EduMathQ2.jsx';
+import EduMathQ3 from './assets/component/page/edu/EduMath/EduMathQ3.jsx';
 
 import './reset.css'
 import './App.css'
@@ -34,10 +36,6 @@ function App() {
           <Route path="/TeamMMs/login" element={<Login />}/>
           <Route path="/TeamMMs/join" element={<Join />}/>
           <Route path="/TeamMMs/mypage" element={<Profile />}/>
-          <Route path="/TeamMMs/" element={<Main />}/>
-          <Route path="/TeamMMs/login" element={<Login />}/>
-          <Route path="/TeamMMs/join" element={<Join />}/>
-          <Route path="/TeamMMs/mypage" element={<Profile />}/>
         </Routes>
       <Footer />
     </>
@@ -48,6 +46,8 @@ function App() {
           <Route path='/education/today' element={<EduToday />} />
           <Route path='/education/today/:unitId' element={<EduTodayCont data={eduContents} qDatas={qDatas} />} />
           <Route path='/education/today/:unitId/1' element={<EduMathQ1 />} />
+          <Route path='/education/today/:unitId/2' element={<EduMathQ2 />} />
+          <Route path='/education/today/:unitId/3' element={<EduMathQ3 />} />
       </Routes>
     </>
     }
---
+++ src/assets/component/page/edu/EduMath/EduMathQ2.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+
+import EduHeader from '../EduHeader';
+import EduMathBlock from './EduMathBlock1';
+import EduFooter from '../EduFooter';
+
+import left_arrow from '../../../../img/icon/left-arrow.png';
+import right_arrow from '../../../../img/icon/right-arrow.png';
+import ask from '../../../../img/icon/info.png';
+import bulb from '../../../../img/icon/bulb.png';
+import check from '../../../../img/icon/check-mark.png';
+import spring from '../../../../img/note_spring.png';
+
+import './EduMathQ.css';
+
+function EduMathQ3(){
+
+    const [onBasic, setOnBasic] = useState(true);
+    const [onDeepQ, setOnDeepQ] = useState(false);
+
+    function onClickBasic(){
+        setOnBasic(true)
+        setOnDeepQ(false)
+    }
+
+    function onClickDeep(){
+        setOnBasic(false)
+        setOnDeepQ(true)
+    }
+    
+    function checkQ(){
+        confirm("채점하시겠습니까?")
+    }
+
+    return(
+        <section className='eduTodaySec'>
+            <EduHeader />
+            <div className='flex eduQBtn'>
+                <div className={onBasic ? "active" : " "} onClick={onClickBasic}>기초문제</div>
+                <div className={onDeepQ ? "active" : " "} onClick={onClickDeep}>응용문제</div>
+            </div>
+            <article className='flex eduUnitWrap'>
+                <div className='eduArrowWrap'>
+                    <Link to={'/education/today/5/1'}>
+                        <img src={left_arrow} alt="" className='eduLeftArrow' />
+                    </Link>
+                    <Link to={'/education/today/5/3'}>
+                        <img src={right_arrow} alt="" className='eduRightArrow' />
+                    </Link>
+                </div>
+                <div className='eduUnitL'>
+                    <h1>02. 다음 입체도형을 보고 모서리와 꼭짓점의 개수를 각각 작성하세요.</h1>
+                    <Canvas
+                        style={{width: `980px`, height: `500px`}}
+                        camera={{fov: 75, near: 0.1, far: 100, position: [2, 2, 2]}}>
+                        <EduMathBlock />
+                    </Canvas>
+                </div>
+                <img src={spring} alt="note-spring" />
+                <div className='flex eduUnitR'>
+                    <div className='eduAnswer'>
+                        <h1>모서리의 개수는?</h1>
+                        <div className='flex'>
+                            <input type="text" />
+                            <p>개</p>
+                        </div>
+                        <h1>꼭짓점의 개수는?</h1>
+                        <div className='flex'>
+                            <input type="text" />
+                            <p>개</p>
+                        </div>
+                    </div>
+                    <div className='flex eduSubBtn'>
+                        <div className='flex askWrap'>
+                            <img src={ask} alt="" />
+                            <h3>질문하기</h3>
+                        </div>
+                        <div className='flex hintWrap'>
+                            <img src={bulb} alt="" />
+                            <h3>힌트보기</h3>
+                        </div>
+                        <div className='flex checkWrap' onClick={checkQ}>
+                            <img src={check} alt="" />
+                            <h3>채점하기</h3>
+                        </div>
+                    </div>
+                </div>
+                <div>{}</div>
+            </article>
+            <EduFooter />
+        </section>
+    )
+}
+
+export default EduMathQ3;

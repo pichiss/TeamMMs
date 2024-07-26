baaeb33e638cd4a057318ec94915824bf659f443
juram <4679jeong@naver.com>
2024-07-26 11:24:12 +0900

    학습현황 찐끝

--- src/assets/component/page/learning/learning.jsx
+++ src/assets/component/page/learning/learning.jsx
@@ -4,13 +4,16 @@ import LearningBarChart from './learningBarChart';
 import LearningDonutChart from './learningDonutChart';
 import LearningScore from './learningScore';
 import LearningAttendance from './learningAttendance';
+import SubHead from '../../common/Subhead';
 
 export default function Learning() {
 
 
 
     return (
-        <section className='w1440 pa55 flex learningWrap'>
+        <>
+        <SubHead/>
+         <section className='w1440 pa55 flex learningWrap'>
             <Subnav tit={'학부모코너'} />
             <div className='learning'>
                 <h2 className='subtit'>학습현황</h2>
@@ -24,7 +27,7 @@ export default function Learning() {
                     <LearningAttendance/>
                 </div>
             </div>
-
         </section>
+        </>
     );
 }

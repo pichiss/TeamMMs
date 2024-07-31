2027effa461bd1d7d81dfdb100b7230d6d325920
keumju <keumju0409@gmail.com>
2024-07-31 12:22:18 +0900

    keumju

--- src/assets/component/common/Subnav.jsx
+++ src/assets/component/common/Subnav.jsx
@@ -1,89 +1,145 @@
 import navNotiIcon from '../../img/icon/noticeIcon.svg';
 import mypageIcon from '../../img/icon/mypageIcon.svg';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import './subnav.css'
 
 function Subnav(props){
+    const navNoti = ['알림나무', navNotiIcon]
+    const myPage = ['학부모코너', mypageIcon]
 
-const navNoti = [
-    {tit:'알림나무',
-    icon: navNotiIcon,
-    key:'1'}]
-const mypage =
-    [{tit:'학부모코너',
-    icon: mypageIcon,
-    key:'2'
-    }]
-const notiList = [
-    {tit:'공지사항&이벤트',
-    key : '1',
-    link : '/noticeList'
-    },
-    {tit:'FAQ',key : '2',
-    link : '/faq'}]
-const mypageList=[
-    {tit:'내 정보',
-    key : '1',
-    link : '/mypage'
-    },
-    {tit:'학습현황',
-    key : '2',
-    link : '/mypage/learning'
-    },
-    {tit:'포인트 내역',
-    key : '3',
-    link : '/mypage/point'
-    },
-    {tit:'1:1 문의',
-    key : '4',
-    link : '/mypage/ask'}]
+    const navNotiList = [
+        {
+            id: 1,
+            tit:'공지사항&이벤트',
+            link : '/noticeList'
+        },
+        {
+            id: 2,
+            tit:'FAQ',
+            link : '/faq'
+        }
+    ]
 
-    const [selectedKey, setSelectedKey] = useState(mypageList[0].key);
+    const myPageList = [
+        {
+            id : '1',
+            tit:'내 정보',
+            link : '/mypage'
+        },
+        {
+            id : '2',
+            tit:'학습현황',
+            link : '/mypage/learning'
+        },
+        {
+            id : '3',
+            tit:'포인트 내역',
+            link : '/mypage/point'
+        },
+        {
+            id : '4',
+            tit:'1:1 문의',
+            link : '/mypage/ask'
+        }
+    ]
 
-    function changeblue(key){
-        setSelectedKey(key);
+
+    let urlName = window.location.href.split('TeamMMs');
+    
+    
+    let [activeColor, setActiveColor] = useState(0);
+    function urls(link, id){
+        useEffect(()=>{       
+            if(urlName[1] === link){
+                console.log(id)
+                setActiveColor(id)
+            }else{
+                null
+            }
+        },[link])
     }
 
 
 
     return(
         <>
-        {props.tit==='알림나무'?  <div>
-        <div className='subnav'>
-            <p>{props.tit}</p>
-            {navNoti.map((list)=>
-            <img src={list.icon} alt={list.icon} key={list.key}></img>
-            )}
+
+
+        {props.tit==='알림나무'?
+        <div>
+            <div className='subnav'>
+                <p>{navNoti[0]}</p>
+                <img src={navNoti[1]} alt={navNoti[0]}/>
+            </div>
+            <div className='subnavList'>
+                <ul className='subnavitem'>
+                    {navNotiList.map((tab)=>
+                        <li key={tab.id} onClick={urls(tab.link, tab.id)} className={activeColor === tab.id ? 'changeblue' : ''}>
+                            <Link to={tab.link}>{tab.tit}</Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
-        <div className='subnavList'>
-            <ul className='subnavitem'>
-                {notiList.map((item)=>
-                    <li className={selectedKey === item.key ? 'changeblue' : null} onClick={() => changeblue(item.key)} key={item.key}>
-                        <Link to={item.link}>{item.tit}</Link>
-                    </li>
-                )}
-            </ul>
+        :
+        <div>
+            <div className='subnav'>
+                <p>{myPage[0]}</p>
+                <img src={myPage[1]} alt={myPage[0]}/>
+            </div>
+            <div className='subnavList'>
+                <ul className='subnavitem'>
+                    {myPageList.map((tab)=>
+                        <li key={tab.id} onClick={urls(tab.link, tab.id)} className={activeColor === tab.id ? 'changeblue' : ''}>
+                            <Link to={tab.link}>{tab.tit}</Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
-        </div>: <div>
-        <div className='subnav'>
-            <p>{props.tit}</p>
-            {mypage.map((list)=>
-            <img src={list.icon} alt={list.icon} key={list.key}></img>
-            )}
-        </div>
-        <div className='subnavList'>
-            <ul className='subnavitem'>
-            {mypageList.map((item)=>
-                <li className={selectedKey === item.key ? 'changeblue' : null}  key={item.key}>
-                    <Link to={item.link} onClick={() => changeblue(item.key)}>{item.tit}</Link>
-                </li>
-            )}
-            </ul>
-        </div>
-        </div>
-    }
-    </>
+        }
+
+
+
+        {/* {props.tit==='알림나무'?  
+            <div>
+                <div className='subnav'>
+                    <p>{props.tit}</p>
+                    {navNoti.map((list)=>
+                        <img src={list.icon} alt={list.icon} key={list.key}/>
+                    )}
+                </div>
+                <div className='subnavList'>
+                    <ul className='subnavitem'>
+                        {notiList.map((item)=>
+                            <li className={selectedKey === item.key ? 'changeblue' : null} onClick={() => changeblue(item.key)} key={item.key}>
+                                <Link to={item.link}>{item.tit}</Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        : 
+            <div>
+                <div className='subnav'>
+                    <p>{props.tit}</p>
+                    {mypage.map((list)=>
+                        <img src={list.icon} alt={list.icon} key={list.key}/>
+                    )}
+                </div>
+                <div className='subnavList'>
+                    <ul className='subnavitem'>
+                    {mypageList.map((item)=>
+                        <li className={selectedKey === item.key ? 'changeblue' : null}  key={item.key}>
+                            <Link to={item.link} onClick={() => changeblue(item.key)}>{item.tit}</Link>
+                        </li>
+                    )}
+                    </ul>
+                </div>
+            </div>
+        } */}
+        </>
     )
 }
 

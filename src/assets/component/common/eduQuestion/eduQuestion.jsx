84e612b09445a4ad33f90467fc2abd94a5b3f564
juram <4679jeong@naver.com>
2024-07-31 09:47:06 +0900

    질문하기 조건문

--- src/assets/component/common/eduQuestion/eduQuestion.jsx
+++ src/assets/component/common/eduQuestion/eduQuestion.jsx
@@ -14,8 +14,14 @@ export default function EduQuestion({ Qclass }) {
   }
 
   function askTeacher() {
-    alert(`질문이 완료되었습니다.`);
-    setOpenAskIcon(!openAskICon);
+    if (askTextRef.current.value < 10 || askTextRef.current.value == null || askTextRef.current.value === undefined) {
+      alert('질문 내용은 10글자 이상으로 입력해주세요.')
+      console.log(askTextRef.current.value)
+     }
+    else {
+      alert(`질문이 완료되었습니다.`);
+      setOpenAskIcon(!openAskICon);
+    }
   }
 
   const asksBtn = {
@@ -37,17 +43,17 @@ export default function EduQuestion({ Qclass }) {
         <div className="askIconPopupWrap">
           <div className="askIconPopup">
             <div className="flex askIconPopupImg">
-            <img src={CloseAsk} alt="닫기" onClick={openAsk} />
+              <img src={CloseAsk} alt="닫기" onClick={openAsk} />
             </div>
             <h5>학습내용에 대해 궁금한 점이 있나요?</h5>
             <div className="flex askIconPopupText">
-            <textarea
-              ref={askTextRef}
-              placeholder="질문을 적어주면, 선생님이 확인 후 답변해 주실거예요."
-            ></textarea>
+              <textarea
+                ref={askTextRef}
+                placeholder="질문을 적어주면, 선생님이 확인 후 답변해 주실거예요."
+              ></textarea>
             </div>
             <div className="flex askIconPopupBtn">
-            <Btn {...asksBtn} func={askTeacher} />
+              <Btn {...asksBtn} func={askTeacher} />
             </div>
           </div>
         </div>

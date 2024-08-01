569e42e74f94296f0dc706084f42d5dedcf65492
keumju <keumju0409@gmail.com>
2024-08-01 12:55:44 +0900

    다시 완료

--- src/assets/page/main/Main.jsx
+++ src/assets/page/main/Main.jsx
@@ -10,7 +10,7 @@ import back from '../../img/background_boss.png'
 import banner from '../../img/mainBanner.png'
 import banner2 from '../../img/mainBanner2.png'
 
-function Main(){
+function Main({user, setPage}){
     // top 버튼
     function scrollToTop(){
         window.scrollTo({
@@ -56,11 +56,19 @@ function Main(){
             <p>TOP</p>
             <img src={top} alt='top btn'/>
         </div>
-        <div className='studyBtn' onClick={scrollToTop}>
-            <Link to='/login'>
-                <p>무료<br/>체험</p>
-            </Link>
-        </div>
+        {user ?
+            <div className='studyBtn' onClick={scrollToTop}>
+                <Link to='/education' onClick={()=>setPage(false)}>
+                    <p>무료<br/>체험</p>
+                </Link>
+            </div>
+        :
+            <div className='studyBtn' onClick={scrollToTop}>
+                <Link to='/login'>
+                    <p>무료<br/>체험</p>
+                </Link>
+            </div>
+        }
     </main>
     )
 }

d7b69eb44783479c22a5331ed41f656aa6c97853
mina <eii1998@naver.com>
2024-08-13 15:27:21 +0900

    pwa

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/TeamMMs/",
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'TeamMMs',
+      short_name: 'MMs',
+      description: 'chatch! mathMonster',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
